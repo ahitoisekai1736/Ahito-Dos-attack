1ac87b212cbb3b60e2203bba520276e6216599e9
ahitoisekai1736 <tesko016@gmail.com>
2025-03-23 11:29:56 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7543594921:AAEb0Qs9usGggKGl0Q-iZ-JimDzJhtv01e0')
+bot = telebot.TeleBot('7769709616:AAEgx8l1ogRyJ53VTP9aSqf4OjK-G0zhjx8')
 
 # Admin user IDs
-admin_id = ["5894556386"]
+admin_id = ["7020353938"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
